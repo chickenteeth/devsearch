d2d356117739a41063ca306b4b6f3a481ed6401c
Tristan Stutzman <tstutzman@turo.com>
2022-08-23 15:32:52 -0700

    authentication

--- users/views.py
+++ users/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Profile
 
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # try:
+        #     user = User.objects.get(username=user)
+        # except:
+        #     messages.error(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username or Password is incorrect')
+
+    return render(request, 'users/login_register.html')
+
+def logoutUser(request):
+    logout(request)
+    messages.info(request, 'User successfully logged out')
+    return redirect('login')
 
 def profiles(request):
     profiles = Profile.objects.all()
@@ -13,3 +46,4 @@ def userProfile(request, pk):
     otherSkills = profile.skill_set.filter(description="")
     context = { 'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills }
     return render(request, 'users/user-profile.html', context)
+    
