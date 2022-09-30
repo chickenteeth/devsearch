1ab3436de7e83f241c50809e4a2355dfc26223aa
Tristan Stutzman <tstutzman@turo.com>
2022-09-30 09:48:09 -0700

    update more class views

--- users/views.py
+++ users/views.py
@@ -114,12 +114,21 @@ class RegisterUser(View):
             return render(request, 'users/login_register.html', context)
 
 
-def profiles(request):
-    profiles, search_query = searchProfiles(request)
-    custom_range, profiles = paginateProfiles(request, profiles, 6)
-    context = {'profiles': profiles, 'search_query': search_query,
-               'custom_range': custom_range}
-    return render(request, 'users/profiles.html', context)
+# def profiles(request):
+#     profiles, search_query = searchProfiles(request)
+#     custom_range, profiles = paginateProfiles(request, profiles, 6)
+#     context = {'profiles': profiles, 'search_query': search_query,
+#                'custom_range': custom_range}
+#     return render(request, 'users/profiles.html', context)
+
+
+class Profiles(View):
+    def get(self, request):
+        profiles, search_query = searchProfiles(request)
+        custom_range, profiles = paginateProfiles(request, profiles, 6)
+        context = {'profiles': profiles, 'search_query': search_query,
+                   'custom_range': custom_range}
+        return render(request, 'users/profiles.html', context)
 
 
 def userProfile(request, pk):
