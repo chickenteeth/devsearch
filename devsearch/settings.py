70a496fcaf2ef7e41969839c361929f5016b388b
Tristan Stutzman <tristan.stutzman@gmail.com>
2022-10-05 00:00:06 -0700

    allowed hosts

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-o8swal#1#=4b(45m@_zu$as@m(r$l(9-ulu(dqmf&*%*kp*w-3
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',
-                 'devsearch.onrender.com', '*.aws.com']
+                 'dev-search-yx0r.onrender.com', '*.aws.com']
 
 
 # Application definition
